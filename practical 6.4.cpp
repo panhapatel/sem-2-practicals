bb3950dc57742cd85f6c3881fdc89fe999bfa25c
panhapatel <patelpanha79@gmail.com>
2026-04-19 15:17:38 +0530

    Add files via upload

---
+++ practical 6.4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+// Base Class
+class Account {
+protected:
+    int accNo;
+    float balance;
+
+public:
+    Account(int a, float b) {
+        accNo = a;
+        balance = b;
+    }
+
+    void deposit(float amt) {
+        balance += amt;
+    }
+
+    void withdraw(float amt) {
+        if (amt <= balance)
+            balance -= amt;
+        else
+            cout << "Insufficient Balance\n";
+    }
+
+    void display() {
+        cout << "Acc No: " << accNo << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+// Savings Class
+class Savings : public Account {
+    float interest;
+
+public:
+    Savings(int a, float b, float i) : Account(a, b) {
+        interest = i;
+    }
+
+    void display() {
+        Account::display();
+        cout << "Interest: " << interest << "%" << endl;
+    }
+};
+
+// Current Class
+class Current : public Account {
+    float overdraft;
+
+public:
+    Current(int a, float b, float o) : Account(a, b) {
+        overdraft = o;
+    }
+
+    void withdraw(float amt) {
+        if (amt <= balance + overdraft)
+            balance -= amt;
+        else
+            cout << "Overdraft limit exceeded\n";
+    }
+
+    void display() {
+        Account::display();
+        cout << "Overdraft: " << overdraft << endl;
+    }
+};
+
+int main() {
+    Savings s(101, 1000, 5);
+    Current c(102, 2000, 500);
+
+    s.deposit(500);
+    s.withdraw(200);
+    s.display();
+
+    cout << endl;
+
+    c.deposit(1000);
+    c.withdraw(2500);
+    c.display();
+
+    return 0;
+}
