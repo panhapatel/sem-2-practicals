fce6ce35e8399de062e562d987cda8002937726b
panhapatel <patelpanha79@gmail.com>
2026-04-22 19:33:40 +0530

    Add files via upload

---
+++ practical 8.4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    string filename;
+    ifstream file;
+
+
+    while (true) {
+        cout << "Enter file name (or type exit): ";
+        cin >> filename;
+
+        if (filename == "exit") {
+            cout << "Program exited.\n";
+            return 0;
+        }
+
+        file.open(filename);
+
+        if (!file) {
+            cout << "Error: File not found! Try again.\n";
+        } else {
+            break;
+        }
+    }
+
+    string line;
+    int lineNumber = 0;
+
+    cout << "\nProcessing file...\n";
+
+
+    while (getline(file, line)) {
+        lineNumber++;
+
+        stringstream ss(line);
+        int num, sum = 0, count = 0;
+        bool valid = true;
+
+
+        while (ss >> num) {
+            sum += num;
+            count++;
+        }
+
+        if (ss.fail() && !ss.eof()) {
+            valid = false;
+        }
+
+        if (valid && count > 0) {
+            cout << "Line " << lineNumber << " → Sum = " << sum
+                 << ", Avg = " << (float)sum / count << endl;
+        } else {
+            cout << "Line " << lineNumber << " → Invalid data, skipped\n";
+        }
+    }
+
+    file.close();
+    return 0;
+}
